f08c8bc7fcb55ca69ffd48bde98c279cc6f3df78
Erick Hernandez <110247498+grpCondusef@users.noreply.github.com>
2025-06-10 12:42:31 -0600

    qrcode_path_archexpedientes

--- DIGITALIZACION_APP/api/helpers/crearPortada.py
+++ DIGITALIZACION_APP/api/helpers/crearPortada.py
@@ -6,7 +6,6 @@ from barcode import Gs1_128
 from barcode.writer import ImageWriter
 from fpdf import FPDF
 
-
 def createFechaApertura(date):
     print(date)
     fechaApertura = ''
@@ -14,13 +13,10 @@ def createFechaApertura(date):
         if i == ' ':
             break
         fechaApertura += i
-    
     # Convertir la cadena a un objeto datetime
     date_obj = datetime.strptime(fechaApertura , '%Y-%m-%d')
-
     # Formatear la fecha en el nuevo formato 'dd-mm-yyyy'
     new_date_str = date_obj.strftime('%d-%m-%Y')    
-    
     return new_date_str
 
 def folioPORI(folio):
@@ -28,11 +24,14 @@ def folioPORI(folio):
         return ''
     else:
         return folio
-    
+
 def create_rectangle(pdf, x, y, width, height):
     pdf.rect(x, y, width, height)
-    
+
 def create_qr_code(expediente_data, path_img):
+    # Asegura que la carpeta existe antes de guardar
+    os.makedirs(path_img, exist_ok=True)
+
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
     qr.add_data(expediente_data.get("clave"))
     qr.make(fit=True)
@@ -42,72 +41,54 @@ def create_qr_code(expediente_data, path_img):
     code = Gs1_128(expediente_data.get("clave"), writer=ImageWriter())
     code.save(f'{path_img}bar_code_{expediente_data.get("clave")}')
 
-
 def get_expedient_data(clave, expediente, vigencia_ids, valoresDocumentales_array, serie_id):
-    
-        # Acceso a las vigencias por su id
-        vigenciaTramite = vigencia_ids.get(1)
-        vigenciaConcentracion = vigencia_ids.get(2)
-        vigenciaTotal = vigencia_ids.get(3)
+    # Acceso a las vigencias por su id
+    vigenciaTramite = vigencia_ids.get(1)
+    vigenciaConcentracion = vigencia_ids.get(2)
+    vigenciaTotal = vigencia_ids.get(3)
 
-        expediente_data = {}
-        expediente_data['clave'] = clave
-        expediente_data['reclamante'] = '' if expediente.reclamante == 'nan' else expediente.reclamante
-        expediente_data['idMacroproceso'] = expediente.idMacroproceso.nombre
-        expediente_data['idProceso'] = expediente.idProceso.nombre
-        
-        expediente_data['valores_documentales'] = "/".join(valoresDocumentales_array)
-        
-        expediente_data['vigenciaTramiteAnios'] = vigenciaTramite.get('anios')
-        expediente_data['vigenciaTramiteMeses'] = vigenciaTramite.get('meses')
-        expediente_data['vigenciaTramiteDias'] = vigenciaTramite.get('dias')
-        
-        expediente_data['vigenciaConcentracionAnios'] = vigenciaConcentracion.get('anios')
-        expediente_data['vigenciaConcentracionMeses'] = vigenciaConcentracion.get('meses')
-        expediente_data['vigenciaConcentracionDias'] = vigenciaConcentracion.get('dias')
-        
-        expediente_data['vigenciaTotalAnios'] = vigenciaTotal.get('anios')
-        expediente_data['vigenciaTotalMeses'] = vigenciaTotal.get('meses')
-        expediente_data['vigenciaTotalDias'] = vigenciaTotal.get('dias')
-        
-        expediente_data['idSerie'] =expediente.idSerie.nombre
-        expediente_data['folioSIO'] = expediente.folioSIO
-        expediente_data['pori'] = expediente.pori
-        expediente_data['fechaApertura'] = expediente.fechaApertura
-        expediente_data['formatoSoporte'] = expediente.formatoSoporte
-        if expediente.idInstitucion:
-            expediente_data['institucion_financiera'] = expediente.idInstitucion.denominacion_social
-        else:
-            expediente_data['institucion_financiera'] = ''
-        expediente_data['resumenContenido'] = expediente.resumenContenido
-        
-        return expediente_data
-
+    expediente_data = {}
+    expediente_data['clave'] = clave
+    expediente_data['reclamante'] = '' if expediente.reclamante == 'nan' else expediente.reclamante
+    expediente_data['idMacroproceso'] = expediente.idMacroproceso.nombre
+    expediente_data['idProceso'] = expediente.idProceso.nombre
+    expediente_data['valores_documentales'] = "/".join(valoresDocumentales_array)
+    expediente_data['vigenciaTramiteAnios'] = vigenciaTramite.get('anios') if vigenciaTramite else ''
+    expediente_data['vigenciaTramiteMeses'] = vigenciaTramite.get('meses') if vigenciaTramite else ''
+    expediente_data['vigenciaTramiteDias'] = vigenciaTramite.get('dias') if vigenciaTramite else ''
+    expediente_data['vigenciaConcentracionAnios'] = vigenciaConcentracion.get('anios') if vigenciaConcentracion else ''
+    expediente_data['vigenciaConcentracionMeses'] = vigenciaConcentracion.get('meses') if vigenciaConcentracion else ''
+    expediente_data['vigenciaConcentracionDias'] = vigenciaConcentracion.get('dias') if vigenciaConcentracion else ''
+    expediente_data['vigenciaTotalAnios'] = vigenciaTotal.get('anios') if vigenciaTotal else ''
+    expediente_data['vigenciaTotalMeses'] = vigenciaTotal.get('meses') if vigenciaTotal else ''
+    expediente_data['vigenciaTotalDias'] = vigenciaTotal.get('dias') if vigenciaTotal else ''
+    expediente_data['idSerie'] = expediente.idSerie.nombre
+    expediente_data['folioSIO'] = expediente.folioSIO
+    expediente_data['pori'] = expediente.pori
+    expediente_data['fechaApertura'] = expediente.fechaApertura
+    expediente_data['formatoSoporte'] = expediente.formatoSoporte
+    if expediente.idInstitucion:
+        expediente_data['institucion_financiera'] = expediente.idInstitucion.denominacion_social
+    else:
+        expediente_data['institucion_financiera'] = ''
+    expediente_data['resumenContenido'] = expediente.resumenContenido
+    return expediente_data
 
 def crear_portada(BASE_DIR, path_img, clave, expediente, vigencia_ids, valoresDocumentales_array, serie_id):
-    
     expediente_data = get_expedient_data(clave, expediente, vigencia_ids, valoresDocumentales_array, serie_id)
-    
     date = str(expediente_data.get("fechaApertura"))
     fechaApertura = createFechaApertura(date)
-    
     folioPori = folioPORI(expediente_data.get("pori"))
 
-    # CREAR CÓDIGO QR
+    # CREAR CÓDIGO QR y CÓDIGO DE BARRAS (Se asegura la carpeta antes de guardar)
     create_qr_code(expediente_data, path_img)
-
-    # CREAR CÓDIGO DE BARRAS
-    code = Gs1_128(expediente_data.get("clave"), writer=ImageWriter())
-    code.save(f'{path_img}bar_code_{expediente_data.get("clave")}')
 
     # CREAMOS EL PDF EN FORMATO LEGAL
     pdf = FPDF(orientation='P', unit='mm', format='Letter')
-    #pdf = PDF(orientation = 'P', unit = 'mm', format='Letter')
     pdf.add_page()  # NECESITAMOS DECLARAR UNA PÁGINA
 
-# ============== PRIMERA SECCIÓN ===============
+    # ============== PRIMERA SECCIÓN ===============
     create_rectangle(pdf, 13, 11, 192.13, 73.1)  # CONTENEDOR PRINCIPAL
-
     create_rectangle(pdf, 13, 11, 53.91, 73.1)   # ======== CONTENEDOR LOGOS =======
     pdf.image(f'{path_img}SHCP_logo.png', x=16, y=16, w=47.51, h=13, link='')
     pdf.image(f'{path_img}CONDUSEF-LOGO.png', x=20, y=34, w=40, h=18, link='')
@@ -141,7 +122,6 @@ def crear_portada(BASE_DIR, path_img, clave, expediente, vigencia_ids, valoresDo
     pdf.set_font('Arial', '', 8)
     pdf.set_xy(92,66)
     pdf.multi_cell(w=109.71, h=6.91, txt=f'{expediente_data.get("idSerie")}', align='C', border=1)
-
 
     #============== SEGUNDA SECCIÓN ===============
     create_rectangle(pdf, 13, 88, 192.13, 82.7) #CONTENEDOR PRINCIPAL
@@ -203,7 +183,6 @@ def crear_portada(BASE_DIR, path_img, clave, expediente, vigencia_ids, valoresDo
     pdf.text(x=145, y=195, txt=f'{expediente_data.get("vigenciaConcentracionAnios")} años {expediente_data.get("vigenciaConcentracionMeses")} meses {expediente_data.get("vigenciaConcentracionDias")} días')
     create_rectangle(pdf, 125, 191.75, 76.5, 4.75)
 
-
     pdf.set_font('Arial', '', 10)
     pdf.text(x=30, y=197, txt='Vigencia')
     pdf.text(x=26, y=201, txt='documental')
@@ -214,7 +193,6 @@ def crear_portada(BASE_DIR, path_img, clave, expediente, vigencia_ids, valoresDo
     pdf.set_font('Arial', '', 9)
     pdf.text(x=107, y=205, txt=f'{expediente_data.get("vigenciaTotalAnios")} años {expediente_data.get("vigenciaTotalMeses")} meses {expediente_data.get("vigenciaTotalDias")} días')
     create_rectangle(pdf, 87, 196.5, 76.8, 5)
-
 
     pdf.set_font('Arial', '', 10)
     pdf.text(x=30, y=211, txt='Volúmen')
@@ -234,7 +212,6 @@ def crear_portada(BASE_DIR, path_img, clave, expediente, vigencia_ids, valoresDo
 
     pdf.set_font('Arial', 'B', 9)
     pdf.text(x=74, y=219.5, txt='Valor Documental')
-    
     create_rectangle(pdf, 48.5, 216, 76.5, 4.75)
     pdf.text(x=149, y=219.5, txt='Formato Soporte')
     create_rectangle(pdf, 125, 216, 76.5, 4.75)
@@ -252,14 +229,13 @@ def crear_portada(BASE_DIR, path_img, clave, expediente, vigencia_ids, valoresDo
     pdf.text(x=23, y=241, txt='De conformidad con el Artículo 214 del Código Penal Federal es delito el desglose de este expediente, así como la sustracción indebida de cualquiera de sus fojas.')
 
     pdf.image(f'{path_img}bar_code_{expediente_data.get("clave")}.png', x=80, y=242, w=76, h=22.28, link='')
-    
-    # Crear las carpetas si no existen
+
+    # Crear las carpetas si no existen para el PDF de portada
     actual_year = datetime.now().year
     actual_month = datetime.now().month
     actual_day = datetime.now().day
-    directory = os.path.join(BASE_DIR, 'archexpedientes/uridec/pdf_portadas/{}/{}/{}/'.format(actual_year, actual_month, actual_day))
+    directory = os.path.join(BASE_DIR, f'archexpedientes/uridec/pdf_portadas/{actual_year}/{actual_month}/{actual_day}/')
     os.makedirs(directory, exist_ok=True)
-    
     pdf.output(f'{directory}{expediente_data.get("clave")}_portada.pdf','F')  # GUARDAMOS EL PDF GENERADO
 
     # Eliminar archivo QR
